ab13b71093c76b806ea4fcfa0dac11059d2f7d95
Bassam Pervez <bassampervez@gmail.com>
2020-07-14 07:12:45 +0530

    last assignment

--- module-5-solution/js/script.js
+++ module-5-solution/js/script.js
@@ -120,7 +120,7 @@ function buildAndShowHomeHTML (categories) {
         console.log(chosenCategoryShortName);    
        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
                      "randomCategoryShortName",
-                     chosenCategoryShortName);
+                     "'"+chosenCategoryShortName+"'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
